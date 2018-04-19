46ebf490766738df783ac3e079efc13b818645d0
LuizPPA <luizphilippep@gmail.com>
2018-04-19 15:31:09 -0300

    Preflight configuration Seting headers and preflight handling to avoid CORS issues.

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,35 @@ const functions = require('firebase-functions')
 const nodemailer = require('nodemailer')
 
 exports.sendMail = functions.https.onRequest((request, response) => {
-  var message = {
-    replyTo: request.body.from, // Address displayed at the "Reply To" field
-    to: 'destination@domain.com', // Destination address
-    subject: "Message from "+request.body.name+": "+request.body.subject, // Message subject
-    text: request.body.message, // Message body as plain text
-    html: '<p>'+request.body.message+'</p>' // Message body as html
+  // Setting response headers
+  response.set('Access-Control-Allow-Origin', "*")
+  response.set('Access-Control-Allow-Methods', 'GET, POST')
+  response.set('Access-Control-Allow-Headers', 'content-type')
+
+  // Allowing preflight requests
+  if (request.method === "OPTIONS") {
+    response.status(200).send("OK")
+    return
+  }
+
+  let from = request.body.from // Email to which reply the message
+  let to = request.body.to // Destination address
+  let name = request.body.name // Sender name
+  let subject = request.body.subject || "No Subject" // Message subject
+  let message = request.body.message // Message text
+  if (from === undefined || name === undefined || subject === undefined || message === undefined || to === undefined) {
+    return response.status(403).send('Please, provide the required info')
+    return
+  }
+  var email = {
+    replyTo: from, // Address displayed at the "Reply To" field
+    to: to, // Destination address
+    subject: "Message from "+name+": "+subject, // Message subject
+    text: message, // Message body as plain text
+    html: '<p>'+message+'</p>' // Message body as html
   }
   var smtpConfig = {
-    host: 'smtp.gmail.com', // Your SMTP host address
+    host: 'smtp.gmail.com', // Your SMTP host address e.g.: Gmail
     port: 587, // Your SMTP port
     secure: false,
     auth: {
@@ -19,6 +39,6 @@ exports.sendMail = functions.https.onRequest((request, response) => {
     }
   }
   let transporter = nodemailer.createTransport(smtpConfig)
-  transporter.sendMail(message)
+  transporter.sendMail(email)
   return response.status(200).send("OK")
 })
